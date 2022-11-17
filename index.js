7b68f128434e8c518e569522722445664baf5e0a
Arnav-JFT <arnav.sharma@jellyfishtechnologies.com>
2022-11-17 15:01:11 +0530

    Added prototype method for counting number of elements

---
+++ index.js
@@ -0,0 +1,16 @@
+let arr = [1,2,3,4,5,6,1,1,1,2,3,6,7]
+
+Array.prototype.countElement = function(element){
+    var count = 0;
+    for(var i=0;i<this.length;i++){
+        if(this[i] === element){
+            count++
+        }
+    }
+
+    return count;
+}
+
+let count = arr.countElement(1)
+
+console.log(count)
