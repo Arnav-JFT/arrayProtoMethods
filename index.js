42b12e4640d177763b0444d268f29d269bebaabf
Arnav-JFT <arnav.sharma@jellyfishtechnologies.com>
2022-11-17 15:17:09 +0530

    Added new Array prototype method

--- index.js
+++ index.js
@@ -10,7 +10,17 @@ Array.prototype.countElement = function(element){
 
     return count;
 }
-
 let count = arr.countElement(1)
+console.log("count is :" , count)
 
-console.log(count)
+
+Array.prototype.typeOfElements = function(){
+    var set = new Set()
+    for(var i =0;i<this.length;i++){
+        set.add(typeof(this[i]))
+    }
+    return set
+}
+var some = [1,2,3,4,5,6,"banana","apple" , {number:"aaa"}]
+
+console.log("This array contains these type of elements",some.typeOfElements())
